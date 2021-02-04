92579f1d599c9ee12e163682ca38dd5693dac111
Hiugo-beep <55299837+Hiugo-beep@users.noreply.github.com>
2021-02-04 15:45:18 +0300

    Update Task03PerformingImpl.java

--- jwd-task-04-client-server/src/main/java/by/jwd/clientserver/dao/impl/tools/impl/Task03PerformingImpl.java
+++ jwd-task-04-client-server/src/main/java/by/jwd/clientserver/dao/impl/tools/impl/Task03PerformingImpl.java
@@ -7,53 +7,44 @@ import java.util.regex.Pattern;
 
 import by.jwd.clientserver.dao.impl.tools.SentenceAggregator;
 import by.jwd.clientserver.dao.impl.tools.TaskPerforming;
+import by.jwd.clientserver.entity.PunctuationMark;
 import by.jwd.clientserver.entity.Sentence;
 import by.jwd.clientserver.entity.StringContent;
+import by.jwd.clientserver.entity.Word;
 
-public class Task03PerformingImpl implements TaskPerforming{
-	
+public class Task04PerformingImpl implements TaskPerforming{
+
 	private List<String> parametrs = null;
 	
 	@Override
 	public List<String> doTask(List<Sentence> sentence) {
 		
 		List<String> answer = new ArrayList<String>();
-		answer.add("words from the first sentence that are not present in the others:\n");
-		Pattern pattern = null;
-		Matcher result = null;
-		boolean hasWord = false;
-		SentenceAggregator aggregator = new SentenceAggregatorImpl();
+		int sizeParametr = Integer.parseInt(parametrs.get(0));
 		
-		List<String> lastWord = new ArrayList<String>();
-		int startIndex = 0;
-
-			Sentence sent = sentence.get(startIndex);
-			
-			String text = aggregator.setSentenceString(sentence, startIndex);
-			
-			for(StringContent word : sent.getSentenceContent()) {
-				boolean isRepeatedWord = lastWord.contains(word.getContent());
-				if(!isRepeatedWord ) {
-					
-					pattern = Pattern.compile(word.getContent());
-					result = pattern.matcher(text);
-					hasWord = result.find();
-					if(!hasWord) {
-						lastWord.add(word.getContent());
-						answer.add(word.getContent());
-						break;
-						
+		for(Sentence sent: sentence) {
+			int index = sent.getSentenceContent().size()-1;
+			boolean isInterrogativeSentence = sent.getSentenceContent().get(index).equals(new PunctuationMark("?"));
+			if(isInterrogativeSentence) {
+				System.out.println("YES ? " + sizeParametr);
+				for(StringContent word:sent.getSentenceContent() ) {
+					boolean isWord = word.getClass() == Word.class;
+					if(isWord) {
+						boolean sizeEqual = sizeParametr == word.getContent().length();
+						if(sizeEqual) {
+							answer.add(word.getContent() + ", ");
+						}
 					}
 				}
 			}
-		
+		}
 	
 		return answer;
 	}
-	
+
 	@Override
 	public void setParametrs(List<String> parametrs) {
 		this.parametrs = parametrs;
-		
 	}
+	
 }

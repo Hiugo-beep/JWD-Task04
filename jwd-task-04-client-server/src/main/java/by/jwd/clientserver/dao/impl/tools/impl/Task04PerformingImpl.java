b598e018512f05fbe166375e51437b59902fd043
Hiugo-beep <55299837+Hiugo-beep@users.noreply.github.com>
2021-02-04 17:06:16 +0300

    Update Task04PerformingImpl.java

--- jwd-task-04-client-server/src/main/java/by/jwd/clientserver/dao/impl/tools/impl/Task04PerformingImpl.java
+++ jwd-task-04-client-server/src/main/java/by/jwd/clientserver/dao/impl/tools/impl/Task04PerformingImpl.java
@@ -7,21 +7,43 @@ import java.util.regex.Pattern;
 
 import by.jwd.clientserver.dao.impl.tools.SentenceAggregator;
 import by.jwd.clientserver.dao.impl.tools.TaskPerforming;
+import by.jwd.clientserver.entity.PunctuationMark;
 import by.jwd.clientserver.entity.Sentence;
 import by.jwd.clientserver.entity.StringContent;
+import by.jwd.clientserver.entity.Word;
 
 public class Task04PerformingImpl implements TaskPerforming{
 
+	private List<String> parametrs = null;
+	
 	@Override
 	public List<String> doTask(List<Sentence> sentence) {
 		
-		List<String> lastWord = new ArrayList<String>();
-
-		
 		List<String> answer = new ArrayList<String>();
-		//answer.add(Integer.toString(counter) + " sentences in the text have repeated words");
+		int sizeParametr = Integer.parseInt(parametrs.get(0));
+		
+		for(Sentence sent: sentence) {
+			int index = sent.getSentenceContent().size()-1;
+			boolean isInterrogativeSentence = sent.getSentenceContent().get(index).equals(new PunctuationMark("?"));
+			if(isInterrogativeSentence) {
+				for(StringContent word:sent.getSentenceContent() ) {
+					boolean isWord = word.getClass() == Word.class;
+					if(isWord) {
+						boolean sizeEqual = sizeParametr == word.getContent().length();
+						if(sizeEqual) {
+							answer.add(word.getContent() + ", ");
+						}
+					}
+				}
+			}
+		}
 	
 		return answer;
 	}
 
+	@Override
+	public void setParametrs(List<String> parametrs) {
+		this.parametrs = parametrs;
+	}
+	
 }

5132aad037d26ef7d25965de803eb5148d543b18
Hiugo-beep <55299837+Hiugo-beep@users.noreply.github.com>
2021-02-04 14:10:29 +0300

    Update CodeBlock.java

--- jwd-task-04-client-server/src/main/java/by/jwd/clientserver/entity/CodeBlock.java
+++ jwd-task-04-client-server/src/main/java/by/jwd/clientserver/entity/CodeBlock.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class CodeBlock implements TextContent, java.io.Serializable{
 
 	private List<StringContent> codeLine = null;
-	public static long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 	
 	public CodeBlock() {
 		codeLine = new ArrayList<StringContent> ();

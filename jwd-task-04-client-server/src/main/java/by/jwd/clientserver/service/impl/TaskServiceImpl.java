6af6a0d21a6772d8c7ca05757cac832ec4bcc63e
Hiugo-beep <55299837+Hiugo-beep@users.noreply.github.com>
2021-02-04 14:17:59 +0300

    Update TaskServiceImpl.java

--- jwd-task-04-client-server/src/main/java/by/jwd/clientserver/service/impl/TaskServiceImpl.java
+++ jwd-task-04-client-server/src/main/java/by/jwd/clientserver/service/impl/TaskServiceImpl.java
@@ -16,82 +16,9 @@ public class TaskServiceImpl implements TaskService {
 		Text parseText = null;
 		
 		DAOFactory factory = DAOFactory.getInstance();
-		TaskDAO taskDAO = null;
-		boolean isValidTaskDAO = true;
-		switch(parametrs.get(0)) {
-		case "1":{
-			taskDAO = factory.getTask01DAO();
-			break;
-		}
-		case "2":{
-			taskDAO = factory.getTask02DAO();
-			break;
-		}
-		case "3":{
-			taskDAO = factory.getTask03DAO();
-			break;
-		}
-		case "4":{
-			taskDAO = factory.getTask04DAO();
-			break;
-		}
-		case "5":{
-			taskDAO = factory.getTask05DAO();
-			break;
-		}
-		case "6":{
-			taskDAO = factory.getTask06DAO();
-			break;
-		}
-		case "7":{
-			taskDAO = factory.getTask07DAO();
-			break;
-		}
-		case "8":{
-			taskDAO = factory.getTask08DAO();
-			break;
-		}
-		case "9":{
-			taskDAO = factory.getTask09DAO();
-			break;
-		}
-		case "10":{
-			taskDAO = factory.getTask10DAO();
-			break;
-		}
-		case "11":{
-			taskDAO = factory.getTask11DAO();
-			break;
-		}
-		case "12":{
-			taskDAO = factory.getTask12DAO();
-			break;
-		}
-		case "13":{
-			taskDAO = factory.getTask13DAO();
-			break;
-		}
-		case "14":{
-			taskDAO = factory.getTask14DAO();
-			break;
-		}
-		case "15":{
-			taskDAO = factory.getTask15DAO();
-			break;
-		}
-		case "16":{
-			taskDAO = factory.getTask16DAO();
-			break;
-		}
-		default:{
-			isValidTaskDAO = false;
-		}
-		}
-		if(isValidTaskDAO) {
-			parametrs.remove(0);
-			parseText = taskDAO.parse(fileText, parametrs);
-		}
+		TaskDAO taskDAO = factory.getTaskDAO();
 		
+		parseText = taskDAO.parse(fileText, parametrs);
 		
 		return parseText;
 	}
